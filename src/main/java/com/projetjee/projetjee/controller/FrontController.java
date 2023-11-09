552659a4d60ac736d8a978bd3ffcbdf520e1d847
kmoser <kmoser@enssat.fr>
2023-11-09 10:08:39 +0100

    Navigation inter-page

--- src/main/java/com/projetjee/projetjee/controller/FrontController.java
+++ src/main/java/com/projetjee/projetjee/controller/FrontController.java
@@ -9,6 +9,33 @@ public class FrontController {
 
     @GetMapping("/")
     public String homePage(){
+        return "connexion";
+    }
+
+    @GetMapping("/index")
+    public String index(){
         return "index";
     }
+
+    @GetMapping("/site")
+    public String site(){
+        return "site";
+    }
+
+    @GetMapping("/disc")
+    public String dicsipline(){
+        return "discipline";
+    }
+
+    @GetMapping("/session")
+    public String session(){
+        return "session";
+    }
+
+    @GetMapping("/stats")
+    public String stats(){
+        return "statistique";
+    }
+
+
 }
