bd4e9af867b4ea6e19fc41963132e3b0f63f47c6
kelig <kvillala@enssat.fr>
2023-11-09 09:42:49 +0100

    Adding first part of the creation form

--- src/main/java/com/projetjee/projetjee/controller/SessionController.java
+++ src/main/java/com/projetjee/projetjee/controller/SessionController.java
@@ -2,9 +2,7 @@ package com.projetjee.projetjee.controller;
 
 import com.projetjee.projetjee.config.jdbcConfig;
 import com.projetjee.projetjee.utils.jdbcUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -17,8 +15,8 @@ import java.util.Map;
 @RestController
 public class SessionController {
 
-    @GetMapping("/createSessionData")
-    public ArrayList<String> sendCreateSessionData(){
+    @GetMapping("/createSessionDisciplines")
+    public ArrayList<String> sendCreateSessionDisciplines(){
         jdbcConfig conf = new jdbcConfig();
         DataSource dataSource = conf.mysqlDataSource();
         try {
@@ -27,9 +25,41 @@ public class SessionController {
             PreparedStatement prep = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
             ResultSet rs = prep.executeQuery();
             Map<String, String>[] queryRes =  jdbcUtils.resultToArray(rs);
+            con.close();
             ArrayList<String> res = new ArrayList<>();
             for(Map<String, String> row : queryRes){
-                System.out.println(row);
+                res.add(row.get("nom"));
+            }
+            return res;
+
+
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+            Map<String, String>[] ret = new Map[1];
+            ret[0] = new HashMap<String, String>();
+            ret[0].put("result","fail");
+            return new ArrayList<>();
+        }
+    }
+
+    @GetMapping("/createSessionEpreuves/{discipline}")
+
+    public ArrayList<String> sendCreateSessionEpreuves(@PathVariable("discipline") String dis){
+        jdbcConfig conf = new jdbcConfig();
+        DataSource dataSource = conf.mysqlDataSource();
+        try {
+            String sql = "SELECT Epreuve.nom FROM Epreuve INNER JOIN Discipline ON Epreuve.id_discipline = Discipline.id_discipline WHERE Discipline.nom = ?";
+            Connection con = dataSource.getConnection();
+            PreparedStatement prep = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            prep.setString(1, dis);
+            ResultSet rs = prep.executeQuery();
+            Map<String, String>[] queryRes =  jdbcUtils.resultToArray(rs);
+            con.close();
+            ArrayList<String> res = new ArrayList<>();
+            for(Map<String, String> row : queryRes){
+                res.add(row.get("nom"));
             }
             return res;
 
---
+++ src/main/java/com/projetjee/projetjee/entities/session.java
@@ -0,0 +1,4 @@
+package com.projetjee.projetjee.entities;
+
+public class session {
+}
